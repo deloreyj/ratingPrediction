27030decccdf372f2925e29ef36e4ea66d07af7b
Siva Mullapudi <scmullap@sivas-mbp-2.attlocal.net>
2017-04-03 21:58:44 -0500

    Clean-up & adding comments

--- data.py
+++ data.py
@@ -1,4 +1,14 @@
-import numpy as np
+"""
+Performs classification to determine a user's rating for a movie.
+
+Given a movie file with attributes and ratings and a user file, train
+several classifiers on these datasets in order to compute a users' ratings
+for a test movie set.
+
+Authors: "James Delorey and Siva Mullapudi"
+Version: Midterm Checkpoint
+"""
+
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,14 +19,19 @@ from sklearn.model_selection import cross_val_score
 
 MISSING_VALUE = "N/A"
 
-def addTransaction(transactionData, trainFeatureMatrix, uniqGenres):
+
+"""
+Converts elements of a transaction and adds them to the feature matrix.
+For now, missing values are given a placeholder -1 value.
+"""
+def addTransaction(transactionData, featureMatrix, uniqGenres):
     gender = transactionData[0]
     age = transactionData[1]
     occupation = transactionData[2]
     year = transactionData[3]
     genre = transactionData[4]
 
-    # Convert male/female to binary value
+    # Convert Male/Female to binary value, -1 if missing
     if gender == "M":
         gender = 1
     elif gender == "F":
@@ -28,6 +43,7 @@ def addTransaction(transactionData, trainFeatureMatrix, uniqGenres):
         age = -1
     else:
         age = int(age)
+
     if occupation == MISSING_VALUE:
         occupation = -1
     else:
@@ -38,13 +54,19 @@ def addTransaction(transactionData, trainFeatureMatrix, uniqGenres):
     else:
         year = int(year)
 
+    # For all possible genres, fill in a 1 if a movie contains that genre
     if genre == MISSING_VALUE:
         genre = [0] * len(uniqGenres)
     else:
         genre = [1 if x in genre else 0 for x in uniqGenres]
 
-    trainFeatureMatrix.append([gender, age, occupation, year] + genre)
+    featureMatrix.append([gender, age, occupation, year] + genre)
 
+"""
+From a training/test file, reads each transaction, expands the ID fields
+to store the features of the corresponding user/movie and appends the feature
+vector to the overall feature matrix.
+"""
 def extractDataAndLabels(filename, usersData, moviesData, uniqGenres):
     featureMatrix = [[]]
     labels = []
@@ -71,7 +93,11 @@ def extractDataAndLabels(filename, usersData, moviesData, uniqGenres):
     else:
         return featureMatrix, testIds
 
+"""
+    Main Function
+"""
 
+# Read the movies file and store a dictionary of the features
 moviesData = {}
 genreSet = set()
 with open('movie.txt', 'r') as movies:
@@ -82,8 +108,8 @@ with open('movie.txt', 'r') as movies:
         moviesData[movieID] = movieData[1:]
         genreSet.update(movieData[-1].split('|'))
 
+# Read the users file and store a dictionary of the features
 usersData = {}
-
 with open('user.txt', 'r') as users:
     next(users)
     for user in users:
@@ -91,63 +117,67 @@ with open('user.txt', 'r') as users:
         userID = userData[0]
         usersData[userID] = userData[1:]
 
+# Read and store the training and test feature matrices
 trainFeatureMatrix, trainLabels = extractDataAndLabels('train.txt', usersData, moviesData, list(genreSet))
+testFeatureMatrix, testIds = extractDataAndLabels('test.txt', usersData, moviesData, list(genreSet))
 
 train_split, test_split, train_split_labels, test_split_labels = train_test_split(trainFeatureMatrix, trainLabels, test_size=0.2)
 
-# Decision Tree
+"""
+Train the classifiers on a single split of the training data and compute accuracy
+"""
+
 dt = tree.DecisionTreeClassifier()
 dt.fit(train_split, train_split_labels)
 dt_score = dt.score(test_split, test_split_labels)
 print "Accuracy of Decision Tree classifier: ", dt_score
 
-cv_dt = tree.DecisionTreeClassifier()
-cv_dt_scores = cross_val_score(cv_dt, trainFeatureMatrix, trainLabels, cv=5)
-print("Cross-Validated Accuracy of Decision Tree: %0.2f (+/- %0.2f)" % (cv_dt_scores.mean(), cv_dt_scores.std() * 2))
-
-# Random Forest
 rf = RandomForestClassifier()
 rf.fit(train_split, train_split_labels)
 rf_score = rf.score(test_split, test_split_labels)
 print "Accuracy of Random Forest classifier: ", rf_score
 
-cv_rf = tree.DecisionTreeClassifier()
-cv_rf_scores = cross_val_score(cv_rf, trainFeatureMatrix, trainLabels, cv=5)
-print("Cross-Validated Accuracy of Random Forest: %0.2f (+/- %0.2f)" % (cv_rf_scores.mean(), cv_rf_scores.std() * 2))
-
-# Gaussian Naive Bayes
 gnb = GaussianNB()
 gnb.fit(train_split, train_split_labels)
 gnb_score = gnb.score(test_split, test_split_labels)
 print "Accuracy of Gaussian Naive Bayes classifier: ", gnb_score
 
-cv_gnb = GaussianNB()
-cv_gnb_scores = cross_val_score(cv_gnb, trainFeatureMatrix, trainLabels, cv=5)
-print("Cross-Validated Accuracy of Gaussian Naive Bayes: %0.2f (+/- %0.2f)" % (cv_gnb_scores.mean(), cv_gnb_scores.std() * 2))
-
-# Bernoulli Naive Bayes
 bnb = BernoulliNB()
 bnb.fit(train_split, train_split_labels)
 bnb_score = bnb.score(test_split, test_split_labels)
 print "Accuracy of Bernoulli Naive Bayes classifier: ", bnb_score
 
-cv_bnb = BernoulliNB()
-cv_bnb_scores = cross_val_score(cv_bnb, trainFeatureMatrix, trainLabels, cv=5)
-print("Cross-Validated Accuracy of Bernoulli Naive Bayes: %0.2f (+/- %0.2f)" % (cv_bnb_scores.mean(), cv_bnb_scores.std() * 2))
-
-# K Nearest Neighbors
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(train_split, train_split_labels)
 knn_score = knn.score(test_split, test_split_labels)
 print "Accuracy of K Nearest Neighbors classifier: ", knn_score
 
+"""
+Train the classifiers with a cross-validated scheme on the whole training set
+"""
+
+cv_dt = tree.DecisionTreeClassifier()
+cv_dt_scores = cross_val_score(cv_dt, trainFeatureMatrix, trainLabels, cv=5)
+print("Cross-Validated Accuracy of Decision Tree: %0.2f (+/- %0.2f)" % (cv_dt_scores.mean(), cv_dt_scores.std() * 2))
+
+cv_rf = RandomForestClassifier()
+cv_rf_scores = cross_val_score(cv_rf, trainFeatureMatrix, trainLabels, cv=5)
+print("Cross-Validated Accuracy of Random Forest: %0.2f (+/- %0.2f)" % (cv_rf_scores.mean(), cv_rf_scores.std() * 2))
+
+cv_gnb = GaussianNB()
+cv_gnb_scores = cross_val_score(cv_gnb, trainFeatureMatrix, trainLabels, cv=5)
+print("Cross-Validated Accuracy of Gaussian Naive Bayes: %0.2f (+/- %0.2f)" % (cv_gnb_scores.mean(), cv_gnb_scores.std() * 2))
+
+cv_bnb = BernoulliNB()
+cv_bnb_scores = cross_val_score(cv_bnb, trainFeatureMatrix, trainLabels, cv=5)
+print("Cross-Validated Accuracy of Bernoulli Naive Bayes: %0.2f (+/- %0.2f)" % (cv_bnb_scores.mean(), cv_bnb_scores.std() * 2))
+
 cv_knn = KNeighborsClassifier(n_neighbors=5)
 cv_knn_scores = cross_val_score(cv_knn, trainFeatureMatrix, trainLabels, cv=5)
 print("Cross-Validated Accuracy of K Nearest Neighbors: %0.2f (+/- %0.2f)" % (cv_knn_scores.mean(), cv_knn_scores.std() * 2))
 
-testFeatureMatrix, testIds = extractDataAndLabels('test.txt', usersData, moviesData, list(genreSet))
 
-# For now, just output test results using Decision Tree Classifier
+# For now, output the test labels using CV Decision Tree Classifier
 predTestLabels = dt.predict(testFeatureMatrix)
 
 testOutput = open('testOutput.txt', 'w')
@@ -156,8 +186,26 @@ for testId, label in zip(testIds, predTestLabels):
     testOutput.write("{},{}\n".format(testId, label))
 testOutput.close()
 
-# users = open('user.txt', 'r')
 
+# """
+# Gather statistics of the movie and user data sets
+# """
+
+# movies = open('movie.txt', 'r')
+# users = open('user.txt', 'r')
+#
+# movieDict = {
+#     'id': {
+#
+#     },
+#     'year': {
+#
+#     },
+#     'genre': {
+#
+#     }
+# }
+#
 # for movie in movies:
 #     moviesLength += 1
 #     splitMovie = movie.strip('\n').split(',')
